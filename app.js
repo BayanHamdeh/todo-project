e3d52437a76ba152d8878524dfec1dac2443693e
BayanHamdeh <b.hamdeh98@gmail.com>
2023-04-19 07:25:23 +0000

    lab6 changes

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
 'use strict'
-// alert("Hi Welcomr to our website");
-// confirm("Do you like our website");
+let userAnswer= [];
 
-// let userAnswer = prompt('what is your name?');
-// console.log('userAnswer >>>', userAnswer);
-
-// prompt ('what is your name?');
-// let userInput = prompt ('what is your name?');
-//اذا بدنا نحول الجملة اللي فوق من string  ل int >> ParseInt
 let name = prompt ('what is your name?');
+// console.log(name);
+userAnswer.push(name);
+console.log(userAnswer);
 
 let gender = prompt ('what is your gender? (male or female)');
-if(gender != 'female' ||  gender != 'male')
+if(gender == 'female' || gender == 'male')
 {
-    alert('gender shoud be either male or female')
+    userAnswer.push(gender);
+}
+else
+{
+    alert('gender shoud either be male or female');
+    userAnswer.push(gender);
+
 }
 
 let age = parseInt(prompt("How old are you"));
 if(age <= 0)
 {
     alert('age shoud not be less than or equal to 0')
+    userAnswer.push(age); 
 }
-// confirm('skip the welcoming message?');
-skip = confirm('skip the welcoming message?');
+else
+{
+    userAnswer.push(age); 
+}
+
+let skip = confirm('skip the welcoming message?');
 if (skip == true)
 {
     console.log(skip)
@@ -40,16 +47,67 @@ else
     {
         confirm("welcome Mr " + name);
     } 
-    
-    
        else {
-        if (gender == null) {
+        if (gender == null) 
+        {
             confirm("welcome " + name);
+            userAnswer.push(gender); 
 
         }
+        else{
+            if(gender != 'female' || gender != 'male' || gender != null)
+            {
+                confirm("welcome " + name);
+                userAnswer.push(gender); 
 
+            }
+        }
     }
-    
 }
 }
 
+let inquiries = confirm("do you have any inquiries?");
+let inq = prompt('you can to put your inquiries here' );
+
+if (inquiries == true) {
+    userAnswer.push(inq);
+
+} 
+   if (inq == null) {
+        console.log("invalid string");
+ }
+
+// function ifEmpty()
+// {
+//     if (inq == null) {
+//         console.log("invalid string");
+//     }
+// }
+// ifEmpty();
+
+let like = confirm("do you have any suggistion?");
+let sug = prompt('you can to put your suggistion here' );
+
+if (like == true) {
+    userAnswer.push(sug);
+}
+// function ifEmpty2()
+// {
+//     if (sug == null) {
+//         console.log("invalid string");
+//     }
+// }
+// ifEmpty2();
+
+// function Traverse()
+// {
+// for (let i = 0; i < userAnswer.length; i++) {
+//     console.log(userAnswer[i]); 
+
+//     return userAnswer[i];
+// }
+// }
+// Traverse(userAnswer[i]);
+for (let i = 0; i < userAnswer.length; i++) {
+    console.log(userAnswer[i]); 
+}
