f6befe324a94a10ea3476629415c5e683a42ecc7
BayanHamdeh <b.hamdeh98@gmail.com>
2023-04-19 10:13:34 +0000

    new lab6

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 'use strict'
-let userAnswer= [];
+let userAnswer = [];
 
-let name = prompt ('what is your name?');
-// console.log(name);
+let name = prompt('what is your name?');
+
 userAnswer.push(name);
 console.log(userAnswer);
 
-let gender = prompt ('what is your gender? (male or female)');
-if(gender == 'female' || gender == 'male')
-{
+let gender = prompt('what is your gender? (male or female)');
+if (gender == 'female' || gender == 'male') {
     userAnswer.push(gender);
 }
-else
-{
+else {
     alert('gender shoud either be male or female');
     userAnswer.push(gender);
 
 }
 
 let age = parseInt(prompt("How old are you"));
-if(age <= 0)
-{
+if (age <= 0) {
     alert('age shoud not be less than or equal to 0')
-    userAnswer.push(age); 
+    userAnswer.push(age);
 }
-else
-{
-    userAnswer.push(age); 
+else {
+    userAnswer.push(age);
 }
 
 let skip = confirm('skip the welcoming message?');
@@ -35,80 +31,68 @@ if (skip == true)
 {
     console.log(skip)
 }
-else
-{
-    if(gender == 'female')
-{
-    confirm("welcome Ms " + name);
-}
+else {
+    if (gender == 'female') {
+        confirm("welcome Ms " + name);
+    }
 
-else
-{
-    if(gender == 'male')
-    {
-        confirm("welcome Mr " + name);
-    } 
-       else {
-        if (gender == null) 
-        {
-            confirm("welcome " + name);
-            userAnswer.push(gender); 
-
+    else {
+        if (gender == 'male') {
+            confirm("welcome Mr " + name);
         }
-        else{
-            if(gender != 'female' || gender != 'male' || gender != null)
-            {
+        else {
+            if (gender == null) {
                 confirm("welcome " + name);
-                userAnswer.push(gender); 
+                userAnswer.push(gender);
 
+            }
+            else {
+                if (gender != 'female' || gender != 'male' || gender != null) {
+                    confirm("welcome " + name);
+                    userAnswer.push(gender);
+
+                }
             }
         }
     }
 }
-}
 
 let inquiries = confirm("do you have any inquiries?");
-let inq = prompt('you can to put your inquiries here' );
+let inq = prompt('you can to put your inquiries here');
 
 if (inquiries == true) {
     userAnswer.push(inq);
 
-} 
-   if (inq == null) {
-        console.log("invalid string");
- }
+}
 
-// function ifEmpty()
-// {
-//     if (inq == null) {
-//         console.log("invalid string");
-//     }
-// }
-// ifEmpty();
+function ifEmpty(n1) {
+    if (n1 == null) {
+        console.log("invalid string");
+    }
+}
+ifEmpty(inq);
 
 let like = confirm("do you have any suggistion?");
-let sug = prompt('you can to put your suggistion here' );
+let sug = prompt('you can to put your suggistion here');
 
 if (like == true) {
     userAnswer.push(sug);
 }
-// function ifEmpty2()
-// {
-//     if (sug == null) {
+// function ifEmpty2(n1) {
+//     if (n1 == null) {
 //         console.log("invalid string");
 //     }
-// }
-// ifEmpty2();
+//}
+ifEmpty(sug);
 
-// function Traverse()
-// {
+function Traverse(arr) {
+    for (let i = 0; i < userAnswer.length; i++) {
+        console.log(arr[i]);
+
+        // return arr[i];
+    }
+}
+Traverse(userAnswer);
 // for (let i = 0; i < userAnswer.length; i++) {
 //     console.log(userAnswer[i]); 
-
-//     return userAnswer[i];
 // }
-// }
-// Traverse(userAnswer[i]);
-for (let i = 0; i < userAnswer.length; i++) {
-    console.log(userAnswer[i]); 
-}
